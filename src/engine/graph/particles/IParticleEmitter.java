5b988556d521b18a9b5c03d3247bf09d74800f62
grabarv <grabar.vladyslav.nl@gmail.com>
2025-08-06 14:43:23 +0200

    create base project structure and add base code for working with openGL

---
+++ src/engine/graph/particles/IParticleEmitter.java
@@ -0,0 +1,13 @@
+package engine.graph.particles;
+
+import java.util.List;
+import engine.items.GameItem;
+
+public interface IParticleEmitter {
+
+    void cleanup();
+    
+    Particle getBaseParticle();
+    
+    List<GameItem> getParticles();
+}
---
+++ src/engine/graph/shadow/ShadowTestRenderer.java
@@ -0,0 +1,52 @@
+package engine.graph.shadow;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.*;
+import engine.Utils;
+import engine.Window;
+import engine.graph.Mesh;
+import engine.graph.ShaderProgram;
+import engine.loaders.assimp.StaticMeshesLoader;
+
+public class ShadowTestRenderer {
+
+    private ShaderProgram testShaderProgram;
+
+    private Mesh quadMesh;
+
+    public void init(Window window) throws Exception {
+        setupTestShader();
+    }
+
+    private void setupTestShader() throws Exception {
+        testShaderProgram = new ShaderProgram();
+        testShaderProgram.createVertexShader(Utils.loadResource("/shaders/test_vertex.vs"));
+        testShaderProgram.createFragmentShader(Utils.loadResource("/shaders/test_fragment.fs"));
+        testShaderProgram.link();
+
+        for (int i = 0; i < ShadowRenderer.NUM_CASCADES; i++) {
+            testShaderProgram.createUniform("texture_sampler[" + i + "]");
+        }
+
+        quadMesh = StaticMeshesLoader.load("/resources/models/quad.obj", "")[0];
+    }
+
+    public void renderTest(ShadowBuffer shadowMap) {
+        testShaderProgram.bind();
+
+        testShaderProgram.setUniform("texture_sampler[0]", 0);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, shadowMap.getDepthMapTexture().getIds()[0]);
+
+        quadMesh.render();
+
+        testShaderProgram.unbind();
+    }
+
+    public void cleanup() {
+        if (testShaderProgram != null) {
+            testShaderProgram.cleanup();
+        }
+    }
+}
